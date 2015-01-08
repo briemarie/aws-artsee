506564336e358521c8bb80864392d58305c41f95
briemarie <thebriemarie@gmail.com>
2015-01-08 10:35:57 -0800

    env file working, uri interpolation working, app fully functional based on search

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -11,17 +11,9 @@ post '/' do
 end
 
 #This is the route for displaying the art from the API
-#Waiting to hear back from Museum about why string interpolation is not allowed in their API URI's, hard coded for now
 get '/:artist' do
 
-  if params[:artist] == "Picasso"
-    redirect "https://www.brooklynmuseum.org/opencollection/api/?method=collection.search&version=1&format=html&api_key=API_KEY&keyword=Picasso"
-
-  elsif params[:artist] == "Monet"
-    redirect "https://www.brooklynmuseum.org/opencollection/api/?method=collection.search&version=1&format=html&api_key=API_KEY&keyword=Monet"
-  else
-      erb :art
-  end
+    redirect "https://www.brooklynmuseum.org/opencollection/api/?method=collection.search&version=1&format=html&api_key=" + "#{ENV['API_KEY']}" + "&keyword=" + "#{params[:artist]}"
 end
 
 
