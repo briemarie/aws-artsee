3ea3d3a87406315df15c6c149f58b5c4aa9e3e23
briemarie <thebriemarie@gmail.com>
2015-01-08 10:59:44 -0800

    Search results now in my app, not redirect

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,7 +13,12 @@ end
 #This is the route for displaying the art from the API
 get '/:artist' do
 
-    redirect "https://www.brooklynmuseum.org/opencollection/api/?method=collection.search&version=1&format=html&api_key=" + "#{ENV['API_KEY']}" + "&keyword=" + "#{params[:artist]}"
+  uri = "https://www.brooklynmuseum.org/opencollection/api/?method=collection.search&version=1&format=html&api_key=" + "#{ENV['API_KEY']}" + "&keyword=" + "#{params[:artist]}"
+
+doc = Nokogiri::HTML(open(uri))
+@images = doc.xpath("//li")
+
+erb :art
 end
 
 
