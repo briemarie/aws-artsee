6a2b22d0893afeb4500ceab87045377be451ee6d
briemarie <thebriemarie@gmail.com>
2015-01-08 18:29:51 -0800

    Starting styling, implement error page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,13 +13,16 @@ end
 #This is the route for displaying the art from the API
 get '/:artist' do
 
-  uri = "https://www.brooklynmuseum.org/opencollection/api/?method=collection.search&version=1&format=html&api_key=" + "#{ENV['API_KEY']}" + "&keyword=" + "#{params[:artist]}"
+  uri = "https://www.brooklynmuseum.org/opencollection/api/?method=collection.search&version=1&format=html&&sort_field=name&image_results_limit=20&api_key=" + "#{ENV['API_KEY']}" + "&keyword=" + "#{params[:artist]}"
 
-doc = Nokogiri::HTML(open(uri))
+  doc = Nokogiri::HTML(open(uri))
+  @art = doc.xpath("//li")
 
-@art = doc.xpath("//li")
-
-erb :art
+  if @art.empty?
+    erb :happy
+  else
+     erb :art
+  end
 end
 
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,18 @@
+
 (function() {
     var dialog = document.getElementById('window');
   document.getElementById('lookup').onclick = function() {
     dialog.show();
   };
 })();
+
+function goBack() {
+    window.history.back()
+}
+
+ $(".button-fill").hover(function () {
+    $(this).children(".button-inside").addClass('full');
+}, function() {
+  $(this).children(".button-inside").removeClass('full');
+});
+
