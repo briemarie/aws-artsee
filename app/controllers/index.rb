030da5d1d53e1c410215c75d18a4f8e8081bb0eb
briemarie <thebriemarie@gmail.com>
2015-03-25 14:58:46 -0700

    No sign up or sign in

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,60 +1,10 @@
-#This is the sign-up page
-get '/session-viewer' do
-  session.inspect
-end
-
-get '/' do
-  if session[:user_id]
-    redirect '/search'
-  end
-
-  erb :sign_up
-end
-
-post '/' do
-  new_user = User.create(email: params[:email], password_hash: params[:password_hash])
-  new_user.password = params[:password_hash]
-  new_user.save
-  session[:user_id] = new_user.id
-  if session[:user_id]
-    redirect '/search'
-  else
-    redirect '/'
-  end
-end
-
-
-#This is the sign-in page (sessions)
-get '/sessions/new' do
-  @user = User.find(session[:user_id])
-
-  session[:user_id] = @user.id
-  erb :sign_in
-end
-
-post '/sessions' do
-  @user = User.find(session[:user_id])
-  if @user.authenticate?(params[:password_hash])
-    session[:user_id] = @user.id
-    redirect '/search'
-  else
-    redirect '/'
-  end
-end
-
-delete '/sessions/:id' do
-  session[:user_id] = nil
-  redirect '/'
-end
-
-
 #This is the profile/search page
-get '/search' do
+get '/' do
 
   erb :profile
 end
 
-post '/search' do
+post '/' do
 
   redirect "/#{params[:artist]}"
 end
